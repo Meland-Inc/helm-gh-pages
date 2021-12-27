beecfd5e6daba589e3d072fae8ce6c2663bd7c06
flyingpig-zzy <68273905+flyingpig-zzy@users.noreply.github.com>
2021-12-27 19:07:17 +0800

    debug

---
+++ debug.sh
@@ -0,0 +1,10 @@
+if [[ $(curl -sIL -w "%{http_code}" --retry 2 -o /dev/null https://meland-inc.github.io/services-charts/index.yaml ) -eq 200 ]]
+    then
+         echo "bbbb:"${exec_code}
+            echo "123123"
+             echo "code:"$?
+    else
+            echo "code:"$?
+            echo "44444"
+    fi;
+
